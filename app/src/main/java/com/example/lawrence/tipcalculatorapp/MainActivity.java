e1307606a8a7c8b44f82ce8fba28b9be8529a6fe
Lawrence L <Lawrence.Lee@alumni.ucsd.edu>
2016-04-04 19:11:23 -0700

    Removed SeekBar. Made tip percent an EditText.

--- app/src/main/java/com/example/lawrence/tipcalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/lawrence/tipcalculatorapp/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.lawrence.tipcalculatorapp;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle; // for saving state information
+import android.os.Bundle;
 
 import android.text.Editable;
 import android.text.TextWatcher;
 
 import android.widget.EditText;
-import android.widget.SeekBar;
-import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -17,86 +15,93 @@ public class MainActivity extends AppCompatActivity {
 
     private static final NumberFormat CURRENCY_FORMAT = NumberFormat.getCurrencyInstance();
     private static final NumberFormat PERCENT_FORMAT = NumberFormat.getPercentInstance();
+    private static final int BASE_PERCENTAGE = 15;
 
     private double mBillAmount = 0.0;
-    private double mPercent = 0.15; // set default tip percentage to 15%
+    private int mPercent = BASE_PERCENTAGE;
 
     // View widgets
     private TextView mAmountTextView;
-    private TextView mPercentTextView;
-    private TextView mTipTextView;
+    private EditText mAmountEditText;
+
+    private EditText mPercentEditText;
+
+    private TextView mTipAmountTextView;
     private TextView mTotalTextView;
 
     // called when the activity is first created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); // call superclass onCreate
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // inflate the GUI
 
-        // get references to widgets
         mAmountTextView = (TextView) findViewById(R.id.amountTextView);
-        mPercentTextView = (TextView) findViewById(R.id.percentTextView);
-        mTipTextView = (TextView) findViewById(R.id.tipTextView);
-        mTotalTextView = (TextView) findViewById(R.id.totalTextView);
-
-        // set tip and total to 0 initially
-        mTipTextView.setText(CURRENCY_FORMAT.format(0));
-        mTotalTextView.setText(CURRENCY_FORMAT.format(0));
 
         // set amountEditText's TextWatcher
-        EditText amountEditText = (EditText) findViewById(R.id.amountEditText);
-        amountEditText.addTextChangedListener(amountEditTextWatcher);
+        mAmountEditText = (EditText) findViewById(R.id.amountEditText);
+        mAmountEditText.addTextChangedListener(amountEditTextWatcher);
 
-        // set percentSeekBar's OnSeekBarChangeListener
-        SeekBar percentSeekBar = (SeekBar) findViewById(R.id.percentSeekBar);
-        percentSeekBar.setOnSeekBarChangeListener(seekBarListener);
+        mPercentEditText = (EditText) findViewById(R.id.percentEditText);
+        mPercentEditText.addTextChangedListener(percentEditTextWatcher);
+
+        // display the amounts in dollars
+        mTipAmountTextView = (TextView) findViewById(R.id.tipTextView);
+        mTipAmountTextView.setText(CURRENCY_FORMAT.format(0));
+
+        mTotalTextView = (TextView) findViewById(R.id.totalTextView);
+        mTotalTextView.setText(CURRENCY_FORMAT.format(0));
     }
 
     // method to calc and display tip and total amounts
     private void calculate() {
-        mPercentTextView.setText(PERCENT_FORMAT.format(mPercent));
 
-        double tip = mBillAmount * mPercent;
+        double percent = mPercent / 100.0;
+        double tip = mBillAmount * percent;
         double total = mBillAmount + tip;
 
-        mTipTextView.setText(CURRENCY_FORMAT.format(tip));
+        mTipAmountTextView.setText(CURRENCY_FORMAT.format(tip));
         mTotalTextView.setText(CURRENCY_FORMAT.format(total));
     }
 
-    // listener object for the SeekBar's progress changed events
-    private final OnSeekBarChangeListener seekBarListener = new OnSeekBarChangeListener() {
+    // listener object for the
+    private final TextWatcher percentEditTextWatcher = new TextWatcher() {
 
-        // update mPercent, then call calculate
         @Override
-        public void onProgressChanged(SeekBar seekBar, int progress,
-                                      boolean fromUser) {
-            mPercent = progress / 100.0; // set mPercent based on progress
-            calculate(); // calculate and display tip and total
+        public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+            try {
+                mPercent = Integer.parseInt(charSequence.toString());
+            } catch (NumberFormatException e) {
+                mPercent = 0;     // if parsing doesn't yield a number, set tip percent to 0.
+            }
+
+            calculate();
         }
 
         @Override
-        public void onStartTrackingTouch(SeekBar seekBar) {
+        public void beforeTextChanged(CharSequence charSequence, int start, int before, int after) {
             // not used
         }
 
         @Override
-        public void onStopTrackingTouch(SeekBar seekBar) {
+        public void afterTextChanged(Editable editable) {
             // not used
         }
-    }; // end seekBarListener object declaration and initialization
+    }; // end object declaration and initialization
 
     // listener object for the EditText's text-changed events
     private final TextWatcher amountEditTextWatcher = new TextWatcher() {
 
         // called when the user modifies the bill amount
         @Override
-        public void onTextChanged(CharSequence s, int start,
-                                  int before, int count) {
+        public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
 
-            try { // get bill amount and display currency formatted value
-                mBillAmount = Double.parseDouble(s.toString()) / 100.0;
+            try {
+                // get bill amount and display currency formatted value
+                mBillAmount = Double.parseDouble(charSequence.toString());
                 mAmountTextView.setText(CURRENCY_FORMAT.format(mBillAmount));
-            } catch (NumberFormatException e) { // if s is empty or non-numeric
+
+            } catch (NumberFormatException e) {
+                // if s is empty or non-numeric
                 mAmountTextView.setText("");
                 mBillAmount = 0.0;
             }
